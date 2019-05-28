527ab4cf058f7a15336102713e0e6d97db800326
Christian <christianeduncan3@gmail.com>
2019-05-28 11:46:54 -0400

    built user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,43 @@ class UsersController < ApplicationController
         @users = User.all 
     end
 
+    def new 
+        @user = User.new 
+    end 
 
+    #create user
+    def create 
+        @user = User.new(user_params)
+        if @user.valid?
+            @user.save
+            flash.now[:notice] = 'User Successfully Created'
+            redirect_to user_path(@user)
+        else
+            flash.now[:notice] = @user.full_messages[0]
+            render :new 
+        end 
+    end 
 
 
     def show
         @user = find_user
     end
+
+    def edit 
+        @user = find_user
+    end 
+
+    def update 
+        @user = find_user 
+        @user.update(user_params)
+        redirect_to user_path(@user)
+    end 
+
+    def destroy
+        @user = find_user 
+        @user.destroy 
+        redirect_to users_path()
+    end 
 
     private
 
@@ -17,4 +48,7 @@ class UsersController < ApplicationController
         User.find(params[:id])
     end
 
+    def user_params
+        params.require(:user).permit(:username)
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # User Routes
-  get "/users", to: "users#index", as: "users"
+  
+  resources :users
 end
