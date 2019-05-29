d854d7d0b14da0874d5b41ca838aec08d1136301
Christian <christianeduncan3@gmail.com>
2019-05-29 10:04:39 -0400

    css nav bar & colors

--- app/models/song.rb
+++ app/models/song.rb
@@ -11,7 +11,7 @@ class Song < ApplicationRecord
             
             if song
                song_result =[]
-               song_result << Song.find_or_create_by(name: song.name, artist: song.artists.first.name)
+               song_result << Song.find_or_create_by(name: song.name, artist: song.artists.first.name, album_url: song.album.images[1]["url"], album: song.album.name)
             else
                 Song.all
                 flash[:notice] = "No song with that name was found"
